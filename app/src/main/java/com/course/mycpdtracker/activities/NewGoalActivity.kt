b9b68a2411894be33313a2e93bbaf5e3c569e89c
Andre Ranussi <ranussi.a@gmail.com>
2023-11-22 10:27:46 +0000

    implementing calendar dialog

--- app/src/main/java/com/course/mycpdtracker/activities/NewGoalActivity.kt
+++ app/src/main/java/com/course/mycpdtracker/activities/NewGoalActivity.kt
@@ -2,12 +2,14 @@ package com.course.mycpdtracker.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import androidx.core.content.ContentProviderCompat.requireContext
 import com.course.mycpdtracker.R
 import com.course.mycpdtracker.databinding.ActivityNewGoalBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+import java.util.TimeZone
 
 class NewGoalActivity : AppCompatActivity() {
 
@@ -37,6 +39,30 @@ class NewGoalActivity : AppCompatActivity() {
 
 
 
+        binding?.tiStartDate?.setEndIconOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker()
+                .setTheme(R.style.DatePickerTheme)
+                .setTitleText("Select a Start Date")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build()
+
+            datePicker.show(this. supportFragmentManager, "TAG")
+
+           datePicker.addOnPositiveButtonClickListener {
+               binding?.etStartDate?.setText(milisescondsToDateFromater(it))
+           }
+
+        }
+
+
+
+    }
+
+    fun milisescondsToDateFromater(miliseconds: Long): String {
+        val utc = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        utc.timeInMillis = miliseconds
+        val format = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        return format.format(utc.timeInMillis)
     }
 
 
